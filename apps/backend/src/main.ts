daa07dc2b61567f813fed3cf703c93f3a31240be
Paulo Marques <paulo.marques@incognito.com>
2026-06-18 11:56:44 -0700

    serveless

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,12 +1,32 @@
-import 'dotenv/config' 
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import serverless from 'serverless-http';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let cachedHandler: any;
+
+async function createServer() {
+  const expressApp = express();
+
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
   app.enableCors({
     origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
   });
-  await app.listen(process.env.PORT ?? 4000);
+
+  await app.init();
+
+  return serverless(expressApp);
 }
-bootstrap();
+
+export default async function handler(req, res) {
+  if (!cachedHandler) {
+    cachedHandler = await createServer();
+  }
+  return cachedHandler(req, res);
+}
