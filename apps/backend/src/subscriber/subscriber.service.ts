2e8ce0d85e0e5a308bdd13704118cc47e0b68c49
Paulo Marques <paulo.marques@incognito.com>
2026-06-18 12:12:02 -0700

    fixing import

--- apps/backend/src/subscriber/subscriber.service.ts
+++ apps/backend/src/subscriber/subscriber.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Subscriber } from '@prisma/client';
-import { PrismaService } from 'src/db/prisma.service';
+import { PrismaService } from '../db/prisma.service';
 
 @Injectable()
 export class SubscriberService {
